cd0587ec5a103f4addac5ea204b452f3026dbe06
Irene <irene@realtimejakarta.com>
2016-01-06 16:24:18 +0700

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ireneProjectAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ireneProjectAngularApp
+ */
+angular.module('ireneProjectAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ireneProjectAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ireneProjectAngularApp
+ */
+angular.module('ireneProjectAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
